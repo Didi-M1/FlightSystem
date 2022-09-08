b1ff5ce0b597283a062f01ef671f167fc8aeec95
Itamar Cohen <itamarit@g.jct.ac.il>
2022-09-08 13:30:42 +0300

    FIx connection strings problem

---
+++ DAL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            AirportsInfo airports = new AirportsInfo();
+            var airport = airports.getAirPortinfo("AAR");
+           
+        }
+    }
+}
