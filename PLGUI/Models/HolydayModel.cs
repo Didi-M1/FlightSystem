777e60a01abe2f5c455690c2f56f224417e8e6d1
Itamar Cohen <itamarit@g.jct.ac.il>
2022-09-11 03:36:50 +0300

    HoliDays VM and M. some poor view

---
+++ PLGUI/Models/HolydayModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  System.Timers;
+using BL;
+using BE.Models;
+namespace PLGUI.Models
+{
+    class HolydayModel
+    {
+        public List<HolydatesInfo> HolydayList { get; set; }        
+        IFlightBL Bl;
+        public HolydayModel()
+        {          
+            Bl = new FlightBL();
+            HolydayList = Bl.isThereAnyHolday(7, DateTime.Now);           
+        }
+        public void updateHolyDayList()
+        {
+            HolydayList = Bl.isThereAnyHolday(7, DateTime.Now);
+        }
+       
+    }
+}
